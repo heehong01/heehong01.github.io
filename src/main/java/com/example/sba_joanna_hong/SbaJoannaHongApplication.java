4846219143bdd858e4d59dc8d7dbe6bf9d3f1dc7
Joanna Hong <heejin.joanna.hong@gmail.com>
2022-04-03 20:08:15 -0500

    sba update

---
+++ src/main/java/com/example/sba_joanna_hong/SbaJoannaHongApplication.java
@@ -0,0 +1,13 @@
+package com.example.sba_joanna_hong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbaJoannaHongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbaJoannaHongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sba_joanna_hong/SbaJoannaHongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sba_joanna_hong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbaJoannaHongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
